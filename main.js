d5b5b3806deb1d16c4f6b546824581e44b35b7e5
Vitaly A. Galinsky <cgsg@yandex.ru>
2023-06-16 09:54:59 +0300

    Correct name & add reconnection code.

--- main.js
+++ main.js
@@ -1,38 +1,32 @@
-// import {$,jQuery} from 'jquery';
-// // export for others scripts to use
-// window.$ = $;
-// window.jQuery = jQuery;
-import * as a from "https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js";
-
-//let $ = require("https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js");
+import * as ___ from "https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js";
 
 function log(msg) {
   $("#log").val($("#log").val() + msg + "\n");
 }
 
-export function myFunc() {
+export function connect() {
   let socket = new WebSocket("wss://cgsg.onrender.com");
   log("my start");
-  socket.onopen = function (e) {
+  socket.onopen = (e) => {
     log("[open] Connection established");
     log("Sending to server");
     socket.send("My name is VG4");
   };
  
-  socket.onmessage = function (event) {
+  socket.onmessage = (event) => {
     log(`[message] Data received from server: ${event.data}`);
   };
   
-  socket.onclose = function (event) {
+  socket.onclose = (event) => {
     if (event.wasClean) {
-      log(
-        `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
-      );
+      log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
     } else {
-      // e.g. server process killed or network down
-      // event.code is usually 1006 in this case
       log("[close] Connection died");
     }
+    log("[close]  Try to reconnect");
+    setTimeout(() => {
+      connect();
+    }, 1000);
   };
  
   socket.onerror = function (error) {
@@ -46,4 +40,4 @@ export function myFunc() {
   });
 
 }
-myFunc();
+connect();
